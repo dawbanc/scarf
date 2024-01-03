d5d1f723d357723c9340155cb2d8503936a0cb2c
DawsonXBancroft <dawsonxbancroft@gmail.com>
2024-01-03 14:33:53 -0800

    Cleaned up logger code

--- src/common/scarf_logger.cpp
+++ src/common/scarf_logger.cpp
@@ -23,22 +23,25 @@ class ScarfLogger{
     bool enable_external_log;
     const char* internal_log_path;
     const char* external_log_path;
+    std::ofstream ext_log;
+    std::ofstream int_log;
 
     public:
         ScarfLogger();
         void setLogLevel(const char* level_in);
         const char* getLogLevel(void);
-        void enableExtLog(void);
         void enableIntLog(void);
-        void setExtLogPath(const char* file_path);
+        void enableExtLog(void);
         void setIntLogPath(const char* file_path);
-        void beginLog(void);
+        void setExtLogPath(const char* file_path);
+        void openLog(void);
+        void closeLog(void);
 };
 
 ScarfLogger::ScarfLogger(){
     level = MEDIUM;                 // set default log level
-    internal_log_path = "null";     // do not log internal log by default
-    external_log_path = "scarf_transcript"; // default external log is scarf_transcript
+    internal_log_path = "null";     // wait to log until paths are set
+    external_log_path = "null";     // wait to log until paths are set
 }
 
 void ScarfLogger::setLogLevel(const char* level_in){
@@ -51,7 +54,8 @@ void ScarfLogger::setLogLevel(const char* level_in){
     } else if (strcmp(level_in,"DEBUG")){
         level = DEBUG;
     } else {
-        std::cout << "scarf-5    \tInternal error: log level incorrectly set." << std::endl;
+        std::cout << "scarf-5    FATAL:\tInternal error: log level incorrectly set." << std::endl;
+        std::cout << "Attempted to put in " << level_in << std::endl;
         exit(5);
     }
 }
@@ -70,24 +74,36 @@ const char* ScarfLogger::getLogLevel(void) {
     return level_out;
 }
 
-void ScarfLogger::enableExtLog(void){
-    enable_external_log = true;
-}
-
 void ScarfLogger::enableIntLog(void){
     enable_internal_log = true;
 }
 
-void ScarfLogger::setExtLogPath(const char* file_path){
-    external_log_path = file_path;
+void ScarfLogger::enableExtLog(void){
+    enable_external_log = true;
 }
 
 void ScarfLogger::setIntLogPath(const char* file_path){
     internal_log_path = file_path;
 }
 
-void ScarfLogger::beginLog(void){
-
+void ScarfLogger::setExtLogPath(const char* file_path){
+    external_log_path = file_path;
 }
 
+void ScarfLogger::openLog(void){
+    if (enable_internal_log) {
+        int_log.open(internal_log_path);
+    }
+    if (enable_external_log) {
+        ext_log.open(external_log_path);
+    }
+}
 
+void ScarfLogger::closeLog(void){
+    if (enable_internal_log) {
+        int_log.close();
+    }
+    if (enable_external_log) {
+        ext_log.close();
+    }
+}
