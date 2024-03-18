3729fc7472fc1d52ab79fc8a13f7f290fa8d1837
dawbanc <mtdrummerboy@gmail.com>
2024-03-18 16:45:07 -0700

    updated math

--- src/scarf/math.cpp
+++ src/scarf/math.cpp
@@ -12,15 +12,14 @@ class Math {
 
 private:
 
-    std::map<std::string, std::string> configuration_value_m;
-    std::map<std::string, std::string> data_value_m;
-    std::map<std::string, std::string> csv_labels_m;
-    std::map<std::string, std::string> csv_math_m;
-
-    std::map<std::string, std::string> constants_m;
-
 
 public:
 
+    void parseMathString(std::string inputMath);
+
 };
 
+void Math::parseMathString(std::string inputMath){
+
+
+}
